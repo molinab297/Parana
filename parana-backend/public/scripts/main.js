bbea476472c746611be06c4200a756fcc1425bdc
Blake Molina <blakeamolina@gmail.com>
2018-07-18 10:22:44 -0700

    Move javascript code from index.html to main.js

--- parana-backend/public/scripts/main.js
+++ parana-backend/public/scripts/main.js
@@ -1,8 +1,18 @@
-// TODO: Logic for the main home page (the shopping page) goes here.
 (function (window) {
   "use strict";
   var $ = window.jQuery;
 
+  dpd.users.me(function(user) {
+    if (user) {
+      $("h1").text("Welcome, " + user.username + "!");
+    }
+  });
+
+  $("#logout-btn").click(function() {
+    dpd.users.logout(function(res, err) {
+      location.href = "/";
+    });
+  });
 
 
 })(window);
