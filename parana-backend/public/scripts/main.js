6972879a02d4ca0b4d096b11df0d5d9fdf425031
Blake Molina <blakeamolina@gmail.com>
2018-07-21 19:50:38 -0700

    Add a bunch of items to the database

--- parana-backend/public/scripts/main.js
+++ parana-backend/public/scripts/main.js
@@ -1,6 +1,6 @@
 (function (window) {
   "use strict";
-  var $ = window.jQuery;
+  const $ = window.jQuery;
 
   dpd.users.me(function(user) {
     if (user) {
@@ -13,6 +13,23 @@
       location.href = "/";
     });
   });
+
+    // Load the items from database onto the home page.
+    dpd.items.get({}, function(results, error) {
+      if (!error){
+          var i = 1;
+          results.forEach(function(item){
+            $("#image" + i).attr('src', item.image);
+            $("#item-title-" + i).text(item.name);
+            $("#item-price-" + i).text("$" + item.price);
+            $("#item-description-" + i).text(item.description);
+            i++;
+          });
+      } else{
+        console.log("Error getting items from database");
+        console.log(error);
+      }
+    });
 
 
 })(window);
